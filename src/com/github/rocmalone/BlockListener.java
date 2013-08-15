61ca44888c2c61e04e97fbb45f5ad778df2e8664
Xander Conger <qwwupp@gmail.com>
2013-08-14 22:51:40 -0700

    Revamped item adding system, added more supported blocks
    BetterMinecraft now supports almost all the blocks in the game,
    excluding a select few and any that have something to do with
    data values (e.g. Lapis Ore is not supported). Attempted to add
    enchantment support, but I'm still working on it.

--- src/com/github/rocmalone/BlockListener.java
+++ src/com/github/rocmalone/BlockListener.java
@@ -18,39 +18,130 @@ public class BlockListener implements Listener {
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
 		Block block = event.getBlock();
-		Material[] dontDrop = new Material[]{Material.STONE, Material.LONG_GRASS};
 		Player p = event.getPlayer();
-		Inventory pInv = p.getInventory();
-		int amnt = 1;
+		ItemStack hand = p.getItemInHand();
 		
 		if (block.getType() == Material.STONE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
-			ItemStack item = new ItemStack(Material.COBBLESTONE, amnt);
-			pInv.addItem(item);
-			if (ctc.isChat == true) {
-				p.sendMessage("§2You Recieved: §f" + Integer.toString(amnt) + " " + block.getType().toString());
-			}
-			
+			methods.checkBlock(p, block, Material.COBBLESTONE, 1, 0);
 		}
+		
 		else if (block.getType() == Material.LONG_GRASS && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
-			if (methods.random(1, 100) > 70) {
-				amnt = methods.random(1, 2);
-				ItemStack item = new ItemStack(Material.SEEDS, amnt);
-				pInv.addItem(item);
-				if (ctc.isChat == true) {
-					p.sendMessage("§2You Recieved: §f" + Integer.toString(amnt) + "SEEDS");
-				}
+			if (methods.random(1, 100) <= 40) {
+				methods.checkBlock(p, block, Material.SEEDS, methods.random(1, 3), 0);
+			}
+		}
+		else if (block.getType() == Material.BOOKSHELF && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.BOOK, 3, 0);
+		}
+		else if (block.getType() == Material.CLAY && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.CLAY_BALL, 4, 0);
+		}
+		else if (block.getType() == Material.DOUBLE_STEP) {
+			methods.checkBlock(p, block, Material.STEP, 2, 0);
+		}
+		else if (block.getType() == Material.GLASS && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.AIR, 0, 0);
+		}
+		else if (block.getType() == Material.THIN_GLASS && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.AIR, 0, 0);
+		}
+		else if (block.getType() == Material.GLOWSTONE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			if (p.getItemInHand().getEnchantments() == Enchantment.LOOT_BONUS_BLOCKS) {
+				methods.checkBlock(p, block, Material.GLOWSTONE, methods.random(4, 6), methods.random(2, 4));
 			}
 			else {
-				
+				methods.checkBlock(p, block, Material.GLOWSTONE, methods.random(2, 4), methods.random(1, 3));
 			}
-			
 		}
-		else {
-			ItemStack item = new ItemStack(block.getType(), amnt);
-			pInv.addItem(item);
-			if (ctc.isChat == true) {
-				p.sendMessage("§2You Recieved: §f" + Integer.toString(amnt) + " " + block.getType().toString());
+		else if (block.getType() == Material.GRASS || block.getType() == Material.SOIL || block.getType() == Material.MYCEL && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.DIRT, 1, 0);
+		}
+		else if (block.getType() == Material.GRAVEL && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			if (p.getItemInHand().getEnchantments() == Enchantment.LOOT_BONUS_BLOCKS) {
+				methods.checkBlock(p, block, Material.FLINT, 1, 0);
 			}
+			else {
+				if (methods.random(1, 100) <= 30) {
+					methods.checkBlock(p, block, Material.FLINT, 1, 0);
+				}
+				else {
+					methods.checkBlock(p, block, Material.GRAVEL, 1, 0);
+				}
+			}
+		}
+		else if (block.getType() == Material.ICE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.ICE, 0, 0);
+		}
+		else if (block.getType() == Material.SNOW_BLOCK && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.SNOW_BALL, 4, 0);
+		}
+		else if (block.getType() == Material.SNOW) {
+			methods.checkBlock(p, block, Material.SNOW_BALL, 1, 0);
+		}
+		else if (block.getType() == Material.COAL_ORE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			if (p.getItemInHand().getEnchantments() == Enchantment.LOOT_BONUS_BLOCKS) {
+				methods.checkBlock(p, block, Material.COAL, methods.random(2, 4), methods.random(2, 4));
+			}
+			else {
+				methods.checkBlock(p, block, Material.COAL, 1, methods.random(1, 3));
+			}
+		}
+		else if (block.getType() == Material.DIAMOND_ORE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			if (p.getItemInHand().getEnchantments() == Enchantment.LOOT_BONUS_BLOCKS) {
+				methods.checkBlock(p, block, Material.DIAMOND, methods.random(2, 4), methods.random(6, 9));
+			}
+			else {
+				methods.checkBlock(p, block, Material.DIAMOND, 1, methods.random(4, 7));
+			}
+		}
+		else if (block.getType() == Material.EMERALD_ORE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			if (p.getItemInHand().getEnchantments() == Enchantment.LOOT_BONUS_BLOCKS) {
+				methods.checkBlock(p, block, Material.EMERALD, methods.random(2, 4), methods.random(6, 9));
+			}
+			else {
+				methods.checkBlock(p, block, Material.EMERALD, 1, methods.random(4, 7));
+			}
+		}
+		/*else if (block.getType() == Material.LAPIS_ORE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			if (p.getItemInHand().getEnchantments() == Enchantment.LOOT_BONUS_BLOCKS) {
+				ItemStack lapis = new ItemStack(351, methods.random(8, 12), (byte)4);
+				pInv.addItem(lapis);
+				expMan.changeExp(methods.random(4,7));
+			}
+			else {
+				ItemStack lapis = new ItemStack(351, methods.random(8, 12), (byte)4);
+				pInv.addItem(lapis);
+				expMan.changeExp(methods.random(3,6));	
+			}
+		}*/
+		else if (block.getType() == Material.QUARTZ_ORE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			if (p.getItemInHand().getEnchantments() == Enchantment.LOOT_BONUS_BLOCKS) {
+				methods.checkBlock(p, block, Material.QUARTZ, methods.random(2, 4), methods.random(3, 6));
+			} 
+			else {
+				methods.checkBlock(p, block, Material.QUARTZ, 1, methods.random(3, 6));
+			}
+		}
+		else if (block.getType() == Material.REDSTONE_ORE || block.getType() == Material.GLOWING_REDSTONE_ORE && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			if (p.getItemInHand().getEnchantments() == Enchantment.LOOT_BONUS_BLOCKS) {
+				methods.checkBlock(p, block, Material.REDSTONE, methods.random(6, 8), methods.random(4, 7));
+			}
+			else {
+				methods.checkBlock(p, block, Material.REDSTONE, methods.random(4, 6), methods.random(2, 6));
+			}
+		}
+		else if (block.getType() == Material.DEAD_BUSH) {
+			methods.checkBlock(p, block, Material.BOOK, 0, 0);
+		}
+		else if (block.getType() == Material.HUGE_MUSHROOM_1 && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.BROWN_MUSHROOM, methods.random(1, 2), 0);
+		}
+		else if (block.getType() == Material.HUGE_MUSHROOM_2 && p.getItemInHand().getEnchantments() != Enchantment.SILK_TOUCH) {
+			methods.checkBlock(p, block, Material.RED_MUSHROOM, methods.random(1, 2), 0);
+		}
+		
+		else {
+			methods.checkBlock(p, block, block.getType(), 1, 0);
 		}
 		event.setCancelled(true);
 		block.setType(Material.AIR);
--- src/com/github/rocmalone/Methods.java
+++ src/com/github/rocmalone/Methods.java
@@ -1,8 +1,36 @@
 package com.github.rocmalone;
 
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
 public class Methods {
+	
+	ChatToggleCommand ctc = new ChatToggleCommand();
+	
 	public int random(int min, int max) {
 		int randNum = min + (int)(Math.random() * ((max - min) + 1));
 		return randNum;
+	}
+	
+	
+	public void checkBlock(Player p, Block block, Material drop, int amnt, int exp) {
+		Inventory pInv = p.getInventory();
+		ItemStack item = new ItemStack(drop, amnt);
+		ExperienceManager expMan = new ExperienceManager(p);
+		if (p.getGameMode() != GameMode.CREATIVE) {
+			pInv.addItem(item);
+			expMan.changeExp(exp);
+			/*if (ctc.isChat == true ) {
+				p.sendMessage("§2You Recieved: §f" + Integer.toString(amnt) + " " + drop.toString());
+			}*/
+		}
+		if (ctc.isChat == true ) {
+			p.sendMessage("§2You Recieved: §f" + Integer.toString(amnt) + " " + drop.toString());
+		}
+		
 	}
 }
